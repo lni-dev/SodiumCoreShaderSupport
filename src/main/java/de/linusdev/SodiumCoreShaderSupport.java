4be6da4422a31bc8bbd941c83c6cc1aab5731a9b
LinusDev <linus@linusdev.de>
2024-03-31 20:23:44 +0200

    updated mc version

--- src/main/java/de/linusdev/SodiumCoreShaderSupport.java
+++ src/main/java/de/linusdev/SodiumCoreShaderSupport.java
@@ -29,52 +29,54 @@ public class SodiumCoreShaderSupport implements ClientModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("sodiumcoreshadersupport");
 
-	public static Map<String, Map<String, Resource>> shaders;
+    public static Map<String, Map<String, Resource>> shaders;
 
-	@Override
-	public void onInitializeClient() {
-		LOGGER.info("Loading shaders...");
+    @Override
+    public void onInitializeClient() {
+        ResourceManagerHelper
+                .get(ResourceType.CLIENT_RESOURCES)
+                .registerReloadListener(
+                        new SimpleResourceReloadListener<Void>() {
 
-		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new SimpleResourceReloadListener<Void>() {
+                            @Override
+                            public Identifier getFabricId() {
+                                return new Identifier("sodium", "shaders");
+                            }
 
-			@Override
-			public Identifier getFabricId() {
-				return new Identifier("sodium", "shaders");
-			}
+                            @Override
+                            public CompletableFuture<Void> load(ResourceManager manager, Profiler profiler, Executor executor) {
+                                return CompletableFuture.supplyAsync(() -> {
+                                    LOGGER.info("Loading shaders...");
+                                    shaders = new HashMap<>();
 
-			@Override
-			public CompletableFuture<Void> load(ResourceManager manager, Profiler profiler, Executor executor) {
-				return CompletableFuture.supplyAsync(() -> {
-					shaders = new HashMap<>();
+                                    manager.findAllResources("shaders", path -> true).forEach((identifier, resources) -> {
+                                        Map<String, Resource> nameSpace = shaders.computeIfAbsent(
+                                                identifier.getNamespace(),
+                                                k -> new HashMap<>()
+                                        );
 
-					manager.findAllResources("shaders", path -> true).forEach((identifier, resources) -> {
-						Map<String, Resource> nameSpace = shaders.computeIfAbsent(
-								identifier.getNamespace(),
-								k -> new HashMap<>()
-						);
+                                        nameSpace.put(
+                                                identifier.getPath().substring("shaders/".length()),
+                                                resources.get(resources.size() - 1)
+                                        );
+                                    });
 
-						nameSpace.put(
-								identifier.getPath().substring("shaders/".length()),
-								resources.get(resources.size() - 1)
-						);
-					});
+                                    shaders.forEach((nameSpace, map) -> {
+                                        System.out.println("nameSpace: " + nameSpace);
+                                        map.forEach((path, resource) -> {
+                                            System.out.println("    " + path + ": " + resource.getResourcePackName());
+                                        });
+                                    });
 
-					shaders.forEach((nameSpace, map) -> {
-						System.out.println("nameSpace: " +nameSpace);
-						map.forEach((path, resource) -> {
-							System.out.println("    " + path + ": " + resource.getResourcePackName());
-						});
-					});
+                                    return null;
+                                }, executor);
+                            }
 
-					return null;
-				}, executor);
-			}
-
-			@Override
-			public CompletableFuture<Void> apply(Void data, ResourceManager manager, Profiler profiler, Executor executor) {
-				return CompletableFuture.runAsync(() -> {
-				}, executor);
-			}
-		});
-	}
+                            @Override
+                            public CompletableFuture<Void> apply(Void data, ResourceManager manager, Profiler profiler, Executor executor) {
+                                return CompletableFuture.runAsync(() -> {
+                                }, executor);
+                            }
+                        });
+    }
 }

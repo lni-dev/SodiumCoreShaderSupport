52549972ab7945c1dea81869592a31dd4a440344
LinusDev <linus@linusdev.de>
2025-08-05 22:10:23 +0200

    Added the SODIUM_CORE_SHADER_SUPPORT define to every shader

---
+++ common/src/main/java/de/linusdev/sodiumcoreshadersupport/mixin/MixinShaderConstants.java
@@ -0,0 +1,23 @@
+package de.linusdev.sodiumcoreshadersupport.mixin;
+
+import net.caffeinemc.mods.sodium.client.gl.shader.ShaderConstants;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.*;
+
+@Mixin(ShaderConstants.Builder.class)
+public abstract class MixinShaderConstants {
+
+
+    @Shadow(remap = false) @Final private HashMap<String, String> constants;
+
+    @Inject(at = @At("HEAD"), method = "build", remap = false)
+    void build(CallbackInfoReturnable<ShaderConstants> cir) {
+        constants.put("SODIUM_CORE_SHADER_SUPPORT", "");
+    }
+}

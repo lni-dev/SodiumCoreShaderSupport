3c00a5f67a9b975fcf950e201c28952cf0129422
LinusDev <linus@linusdev.de>
2025-10-03 19:20:39 +0200

    added u_GameTime uniform to sodium chunk shaders

---
+++ common/src/main/java/de/linusdev/sodiumcoreshadersupport/mixin/MixinDefaultShaderInterface.java
@@ -0,0 +1,38 @@
+package de.linusdev.sodiumcoreshadersupport.mixin;
+
+import net.caffeinemc.mods.sodium.client.gl.shader.uniform.GlUniformFloat;
+import net.caffeinemc.mods.sodium.client.render.chunk.shader.ChunkShaderOptions;
+import net.caffeinemc.mods.sodium.client.render.chunk.shader.DefaultShaderInterface;
+import net.caffeinemc.mods.sodium.client.render.chunk.shader.ShaderBindingContext;
+import net.caffeinemc.mods.sodium.client.render.chunk.terrain.TerrainRenderPass;
+import net.caffeinemc.mods.sodium.client.util.FogParameters;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DefaultShaderInterface.class)
+public abstract class MixinDefaultShaderInterface {
+
+    @Unique
+    private GlUniformFloat sodiumCoreShaderSupport$uniformGameTime = null;
+
+    @Inject(at = @At("RETURN"), method = "<init>", remap = false)
+    private void injectConstructor(ShaderBindingContext context, ChunkShaderOptions options, CallbackInfo ci) {
+        sodiumCoreShaderSupport$uniformGameTime = context.bindUniform("u_GameTime", GlUniformFloat::new);
+    }
+
+    @Inject(at = @At("RETURN"), method = "setupState", remap = false)
+    private void injectSetupState(TerrainRenderPass pass, FogParameters parameters, CallbackInfo ci) {
+        Minecraft minecraft = Minecraft.getInstance();
+        long time = minecraft.level == null ? 0L : minecraft.level.getGameTime();
+        DeltaTracker deltaTracker = minecraft.getDeltaTracker();
+        sodiumCoreShaderSupport$uniformGameTime.set(
+                ((float)(time % 24000L) + deltaTracker.getGameTimeDeltaPartialTick(false)) / 24000.0F
+        );
+    }
+
+}
